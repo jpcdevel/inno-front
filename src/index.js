cf82612c8fd99912f912e16a8416bea8e050de8a
jpcdevel <sashakovalenko96@yandex.ru>
2021-12-02 15:38:59 +0300

    init;2C

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ApolloProvider, ApolloClient, InMemoryCache, from, HttpLink } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import { setContext } from '@apollo/client/link/context';
+
+// APOLLO CONFIGURATION
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+      if (graphQLErrors[0].message == 'Signature has expired') {
+          window.location.reload();
+          if (localStorage.getItem("reloaded") == false) {
+              window.location.reload();
+              localStorage.setItem("reloaded", true)
+          } else {
+              console.log("not exp")
+          }
+      }
+      console.log('graphQLErrors', graphQLErrors);
+  }
+  if (networkError) {
+      console.log('networkError', networkError);
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: 'http://localhost:8000/graphql', credentials: 'include' }) // http://localhost:8000/graphql // https://lycnet4.herokuapp.com/graphql
+])
+
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('accessToken');
+  // return the headers to the context so httpLink can read them
+  return {
+      headers: {
+          ...headers,
+          authorization: token ? `JWT ${token}` : "",
+      }
+  }
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(link),
+})
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+        {" "}
+        <React.StrictMode>
+              <App />
+        </React.StrictMode>
+    </ApolloProvider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
